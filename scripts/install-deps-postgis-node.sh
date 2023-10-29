17973d46cada24d469a59a3e2c52e62c2e32ac29
Ville Säävuori <ville@unessa.net>
2023-10-29 13:15:01 +0200

    chore: bump node version to 20

--- scripts/install-deps-postgis-node.sh
+++ scripts/install-deps-postgis-node.sh
@@ -6,7 +6,7 @@ apt -y upgrade
 apt -y install --no-install-recommends apt-utils build-essential libpq-dev binutils libproj-dev gdal-bin curl ca-certificates dirmngr software-properties-common gnupg gnupg2 apt-transport-https wget ffmpeg
 wget --quiet -O - https://www.postgresql.org/media/keys/ACCC4CF8.asc | apt-key add -
 sh -c 'echo "deb http://apt.postgresql.org/pub/repos/apt $(lsb_release -cs)-pgdg main" > /etc/apt/sources.list.d/pgdg.list'
-curl -fsSL https://deb.nodesource.com/setup_18.x | bash -
+curl -fsSL https://deb.nodesource.com/setup_20.x | bash -
 apt update 
 apt-get install -y nodejs postgresql-client
 ./install-audiowaveform.sh
